011c85ba0d7cc6e14c82656f0508cb66d6ba4935
bright04221 <80819227+bright04221@users.noreply.github.com>
2021-06-25 20:46:52 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://bright04221.github.io/sound10732630/model.json';
 
 
 function preload() {
